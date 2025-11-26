798a7620678ed049d1c3307a7f904de22a6f6cd6
Jens Vogt <jens.vogt@opitz-consulting.com>
2025-11-26 14:37:13 +0100

    fix(first-release): add default FTP user/password

--- src/modules/ftp/CurlHandle.cpp
+++ src/modules/ftp/CurlHandle.cpp
@@ -1,22 +1,20 @@
-#include "CurlHandle.h"
 
 #include <curl/curl.h>
 #include <stdexcept>
 
+#include <modules/ftp/CurlHandle.h>
+
 namespace embeddedmz {
-
-CurlHandle::CurlHandle() {
-   const auto eCode = curl_global_init(CURL_GLOBAL_ALL);
-   if (eCode != CURLE_OK) {
-      throw std::runtime_error{"Error initializing libCURL"};
+   CurlHandle::CurlHandle() {
+      if (const auto eCode = curl_global_init(CURL_GLOBAL_ALL); eCode != CURLE_OK) {
+         throw std::runtime_error{"Error initializing libCURL"};
+      }
    }
-}
 
-CurlHandle::~CurlHandle() { curl_global_cleanup(); }
+   CurlHandle::~CurlHandle() { curl_global_cleanup(); }
 
-CurlHandle &CurlHandle::instance() {
-   static CurlHandle inst{};
-   return inst;
-}
-
-}  // namespace embeddedmz
+   CurlHandle &CurlHandle::instance() {
+      static CurlHandle inst{};
+      return inst;
+   }
+} // namespace embeddedmz
