bb46f86da106889865334e886d2d1ab5f3fff0b9
jensv <jens.vogt@opitz-consulting.com>
2025-11-28 10:28:00 +0100

    fix: update version string in Windows installer

--- include/Version.h
+++ include/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define APP_VERSION "1.4.19"
+#define APP_VERSION "1.4.25"
 #define APP_VERSION_MAJOR 1
 #define APP_VERSION_MINOR 4
-#define APP_VERSION_PATCH 19
+#define APP_VERSION_PATCH 25
