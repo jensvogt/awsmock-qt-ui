852bee1e5ce760b1ec8f03949a70f99b588e030b
Jens Vogt <jens.vogt@opitz-consulting.com>
2025-12-11 16:06:35 +0100

    fix: fix SQS queue details dialog layout, SQS message dialog layout

--- src/modules/lambda/LambdaLogsDialog.cpp
+++ src/modules/lambda/LambdaLogsDialog.cpp
@@ -6,18 +6,25 @@
 
 #include <modules/lambda/LambdaLogsDialog.h>
 #include "ui_LambdaLogsDialog.h"
+#include "utils/StringUtils.h"
 
 LambdaLogsDialog::LambdaLogsDialog(const QString &oid, QWidget *parent) : QDialog(parent), _ui(new Ui::LambdaLogsDialog) {
 
     // Lambda REST service
     _lambdaService = new LambdaService();
+    connect(_lambdaService, &LambdaService::GetLambdaResultSignal, this, &LambdaLogsDialog::HandleGetResult);
 
     _ui->setupUi(this);
     connect(_ui->buttonBox, &QDialogButtonBox::accepted, this, &LambdaLogsDialog::HandleAccept);
     connect(_ui->buttonBox, &QDialogButtonBox::rejected, this, &LambdaLogsDialog::HandleReject);
 
+    // Pretty print
+    _ui->requestPrettyButton->setText(nullptr);
+    _ui->requestPrettyButton->setIcon(IconUtils::GetIcon("pretty"));
+    connect(_ui->requestPrettyButton, &QPushButton::toggled, this, &LambdaLogsDialog::HandleRequestPrettyButton);
+
     // Get lambda Logs
-    //_lambdaService->GetLambdaResult(oid);
+    _lambdaService->GetLambdaResult(oid);
 }
 
 LambdaLogsDialog::~LambdaLogsDialog() {
@@ -30,4 +37,36 @@ void LambdaLogsDialog::HandleAccept() {
 
 void LambdaLogsDialog::HandleReject() {
     accept();
+}
+
+void LambdaLogsDialog::HandleGetResult(const LambdaGetResultsResponse &lambdaLogsResult) const {
+
+    // Header
+    _ui->regionEdit->setText(lambdaLogsResult.lambdaLogCounter.region);
+    _ui->functionEdit->setText(lambdaLogsResult.lambdaLogCounter.lambdaName);
+    _ui->timestampEdit->setText(lambdaLogsResult.lambdaLogCounter.timestamp.toString("yyyy-MM-dd hh:mm:ss"));
+
+    // Tabs
+    _ui->requestEdit->setText(lambdaLogsResult.lambdaLogCounter.requestBody);
+    _ui->outputEdit->setText(lambdaLogsResult.lambdaLogCounter.responseBody);
+    _ui->logsEdit->setText(StringUtils::CleanupLogString(lambdaLogsResult.lambdaLogCounter.logMessages));
+}
+
+void LambdaLogsDialog::HandleRequestPrettyButton(const bool checked) const {
+    if (checked) {
+        const QByteArray body = _ui->requestEdit->toPlainText().toUtf8();
+        const QJsonDocument jDoc = QJsonDocument::fromJson(body);
+        _ui->requestEdit->clear();
+        _ui->requestEdit->setPlainText(jDoc.toJson(QJsonDocument::Indented));
+    } else {
+        const QByteArray body = _ui->requestEdit->toPlainText().toUtf8();
+        const QJsonDocument jDoc = QJsonDocument::fromJson(body);
+        _ui->requestEdit->clear();
+        _ui->requestEdit->setPlainText(jDoc.toJson(QJsonDocument::Compact));
+    }
+    _ui->requestEdit->updateGeometry();
+    QTextCursor cursor = _ui->requestEdit->textCursor();
+    cursor.movePosition(QTextCursor::End);
+    _ui->requestEdit->setTextCursor(cursor);
+    _ui->requestEdit->ensureCursorVisible();
 }
