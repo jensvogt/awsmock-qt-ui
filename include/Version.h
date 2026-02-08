eef7cb27d8f3df35a56ef1a6bee984fc1128bed7
Jens Vogt <jens.vogt@opitz-consulting.com>
2026-02-08 03:00:11 +0100

    fix: add versioning

--- include/Version.h
+++ include/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define APP_VERSION "1.10.6"
+#define APP_VERSION "1.10.7"
 #define APP_VERSION_MAJOR 1
 #define APP_VERSION_MINOR 10
-#define APP_VERSION_PATCH 6
+#define APP_VERSION_PATCH 7
---
+++ include/utils/UpdateChecker.h
@@ -0,0 +1,57 @@
+//
+// Created by vogje01 on 2/8/26.
+//
+
+#ifndef AWSMOCK_QT_UI_UPDATECHECKER_H
+#define AWSMOCK_QT_UI_UPDATECHECKER_H
+
+// Qt includes
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QCoreApplication>
+#include <QVersionNumber>
+
+// Awsmock includes
+#include <Version.h>
+
+class UpdateChecker : public QObject {
+    Q_OBJECT
+
+public:
+    UpdateChecker(QObject *parent = nullptr) : QObject(parent) {
+    }
+
+    void checkForUpdates() {
+        const QUrl url("https://jensvogt.github.io/awsmock-qt-ui/version.txt");
+        const QNetworkRequest request(url);
+
+        QNetworkReply *reply = manager.get(request);
+        connect(reply, &QNetworkReply::finished, this, [this, reply]() {
+            if (reply->error() == QNetworkReply::NoError) {
+                const QString latestVersion = QString(reply->readAll()).trimmed();
+                compareVersions(latestVersion);
+            }
+            reply->deleteLater();
+        });
+    }
+
+private:
+    void compareVersions(const QString &latest) {
+        QVersionNumber currentV = QVersionNumber::fromString(APP_VERSION);
+        QVersionNumber latestV = QVersionNumber::fromString(latest);
+
+        if (latest > currentVersion) {
+            qInfo() << "Update Available! Current:" << currentVersion << "Latest:" << latest;
+            emit UpdateAvailable(latest);
+        } else {
+            qInfo() << "You are up to date.";
+        }
+    }
+
+    QNetworkAccessManager manager;
+    QString currentVersion;
+
+signals:
+    void UpdateAvailable(const QString &ver);
+};
+#endif //AWSMOCK_QT_UI_UPDATECHECKER_H
